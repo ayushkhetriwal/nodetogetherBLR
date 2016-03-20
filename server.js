ccfa3b88764fde16f58f342baf3ad69bb90f7b8d
Ayush Khetriwal <ayushkhetriwal9993@gmail.com>
2016-03-20 16:57:55 +0530

    modifying server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const port = process.env.PORT || '8080';
-const host = process.env.HOST || '0.0.0.0';
-const express = require('express');
-const app = express();
+// const port = process.env.PORT || '8080';
+// const host = process.env.HOST || '0.0.0.0';
+// const express = require('express');
+// const app = express();
 
-app.use(require('./app'));
+// app.use(require('./app'));
 
-app.listen(port, host);
+// app.listen(port, host);
 
-console.log('Server running %s:%d...', host, port);
+// console.log('Server running %s:%d...', host, port);
 
-module.exports = app;
+// module.exports = app;
+
+
+var port = "8080";
+var host = '0.0.0.0';
+
+var express = require('express');
+var app = express();
+
+app.use(require,('./app'));
+
+app.listen(port,host);
+
+console.log("Server is listening ..... ");
