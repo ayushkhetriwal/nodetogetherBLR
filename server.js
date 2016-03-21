bbd27f56bc59aebefdb87217db05673db2b76a65
Himanshu Hiranandani <hhiranandani@deloitte.com>
2016-03-21 11:31:13 +0530

    updated listen ports

--- server.js
+++ server.js
@@ -22,7 +22,12 @@ var pickone = require('pick-one');
 
 app.use(require('./app'));
 
-app.listen(port,host);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 
 console.log("Server is listening ..... ");
 
